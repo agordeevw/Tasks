6d3adc0396d1960d52acacadf908e82f4e6eec33
agordeevw <agordeevw@gmail.com>
2018-09-28 10:50:58 +0700

    Implemented sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,19 @@
-// TODO
+__kernel void sum(
+    __global const unsigned int* values,
+    __global unsigned int* partial_sums,
+    __local unsigned int* local_values)
+{
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_size = get_local_size(0);
+
+    local_values[local_id] = values[get_global_id(0)];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (unsigned int stride = group_size / 2; stride > 0; stride /= 2) {
+        if (local_id < stride)
+            local_values[local_id] += local_values[local_id + stride];
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (local_id == 0)
+        partial_sums[get_group_id(0)] = local_values[0];
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,9 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -57,8 +60,56 @@ int main(int argc, char **argv)
         std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
+    try
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+        ocl::Kernel kernel(sum_kernel, sum_kernel_length, "sum");
+        kernel.compile(true);
+
+        const unsigned int work_group_size = 128;
+
+        auto round_up_work_size = [&work_group_size](unsigned int work_size) {
+            return (work_size + work_group_size - 1) / work_group_size * work_group_size;
+        };
+
+        auto work_groups_count = [&round_up_work_size, &work_group_size](unsigned int work_size) {
+            return round_up_work_size(work_size) / work_group_size;
+        };
+
+        gpu::gpu_mem_32u buffer_data;
+        buffer_data.resizeN(n);
+        buffer_data.writeN(as.data(), as.size());
+        gpu::gpu_mem_32u buffer_result;
+        buffer_result.resizeN(work_groups_count(n));
+
+        unsigned int sum;
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            unsigned int values_count = n;
+            gpu::WorkSize work_size{ work_group_size, round_up_work_size(values_count) };
+            ocl::LocalMem local_values{ sizeof(unsigned int) * work_group_size };
+            kernel.exec(work_size, buffer_data, buffer_result, local_values);
+                     
+            t.nextLap();
+        }
+
+        sum = 0;
+        std::vector<unsigned int> results(work_groups_count(n));
+        buffer_result.readN(results.data(), results.size());
+        for (auto result : results)
+            sum += result;
+
+        EXPECT_THE_SAME(reference_sum, sum, "GPU result");
+
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 }
