ea695f86072f297732dac7761551fecef77c11f0
agordeevw <agordeevw@gmail.com>
2018-09-29 11:45:46 +0700

    Implemented task 3

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,105 @@
-// TODO
+__kernel void zero_buffer(
+    __global int* buffer
+)
+{
+    buffer[get_global_id(0)] = 0;
+}
+
+// --------------------------------------------------------------
+
+__kernel void scan(
+    __global const int* input,
+    __global int* output,
+    __global int* sums,
+    __local int* locals)
+{
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_size = get_local_size(0);
+    const unsigned int global_id = get_global_id(0);
+
+    locals[local_id] = input[global_id];
+
+    int offset = 1;
+
+    for (int level = group_size / 2; level > 0; level /= 2) {
+      barrier(CLK_LOCAL_MEM_FENCE);
+      if (local_id < level) {
+        int dst_i = (2 * local_id + 2) * offset - 1;
+        int src_i = (2 * local_id + 1) * offset - 1;
+        locals[dst_i] += locals[src_i];
+      }
+      offset *= 2;
+    }
+
+    if (local_id == 0) {
+      if (sums)
+        sums[get_group_id(0)] = locals[group_size - 1];
+      locals[group_size - 1] = 0;
+    }
+
+    for (int level = 1; level < group_size; level *= 2) {
+      offset /= 2;
+      barrier(CLK_LOCAL_MEM_FENCE);
+      if (local_id < level) {
+        int dst_i = (2 * local_id + 2) * offset - 1;
+        int src_i = (2 * local_id + 1) * offset - 1;
+        int t = locals[src_i];
+        locals[src_i] = locals[dst_i];
+        locals[dst_i] += t;
+      }
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id + 1 < group_size)
+      output[global_id] = locals[local_id + 1];
+    else
+      output[global_id] = locals[local_id] + input[global_id];
+}
+
+// --------------------------------------------------------------
+
+__kernel void add(
+    __global int* scanned_input,
+    __global const int* sums
+)
+{
+    if (get_group_id(0) != 0)
+      scanned_input[get_global_id(0)] += sums[get_group_id(0) - 1];
+}
+
+// --------------------------------------------------------------
+
+__kernel void get_max_value(
+    __global const int* data,
+    __global int* ret,
+    __local int* idxs
+)
+{
+    const unsigned int local_id = get_local_id(0);
+
+    idxs[local_id] = get_global_id(0);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (unsigned int stride = get_local_size(0) / 2; stride > 0; stride /= 2) {
+        if (local_id < stride) {
+            if (data[idxs[local_id]] < data[idxs[local_id + stride]])
+                idxs[local_id] = idxs[local_id + stride];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (local_id == 0)
+        atomic_max(ret, data[idxs[0]]);
+}
+
+// --------------------------------------------------------------
+
+__kernel void get_idx_of_max(
+    __global const int* data,
+    __global const int* max,
+    __global int* ret
+) 
+{
+    if (data[get_global_id(0)] == max[0]) {
+        atomic_min(ret, (int)get_global_id(0) + 1);
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,7 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
-
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include "cl/max_prefix_sum_cl.h"
+#include <fstream>
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -17,6 +20,32 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    ocl::Kernel scan, add, zero_buffer, get_max_value, get_idx_of_max;
+    try {
+        scan.init(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "scan");
+        scan.compile(true);
+
+        add.init(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "add");
+        add.compile(true);
+
+        zero_buffer.init(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "zero_buffer");
+        zero_buffer.compile(true);
+
+        get_max_value.init(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "get_max_value");
+        get_max_value.compile(true);
+
+        get_idx_of_max.init(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "get_idx_of_max");
+        get_idx_of_max.compile(true);
+    }  catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }    
+
     int benchmarkingIters = 10;
     int max_n = (1 << 24);
 
@@ -70,8 +99,116 @@ int main(int argc, char **argv)
             std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
 
-        {
+        try {
             // TODO: implement on OpenCL
+            // Reference:
+            // https://developer.nvidia.com/gpugems/GPUGems3/gpugems3_ch39.html
+
+            const unsigned int work_group_size = 128;
+
+            auto round_up_work_size = [&work_group_size](unsigned int work_size) {
+                return (work_size + work_group_size - 1) / work_group_size * work_group_size;
+            };
+
+            auto work_groups_count = [&round_up_work_size, &work_group_size](unsigned int work_size) {
+                return round_up_work_size(work_size) / work_group_size;
+            };
+
+            gpu::gpu_mem_32i buffer_input;
+            buffer_input.resizeN(round_up_work_size(n));
+            zero_buffer.exec(gpu::WorkSize{ work_group_size, buffer_input.number() }, buffer_input);
+            buffer_input.writeN(as.data(), as.size());
+
+            gpu::gpu_mem_32i buffer_output;
+            buffer_output.resizeN(round_up_work_size(n));
+
+            std::vector<gpu::gpu_mem_32i> buffers_sums, buffers_scanned_sums;
+            unsigned int buffer_size = n;
+            do {
+                buffer_size = round_up_work_size(work_groups_count(buffer_size));
+
+                buffers_sums.emplace_back();
+                auto& buffer_sums = buffers_sums.back();
+                buffer_sums.resizeN(buffer_size);
+                zero_buffer.exec(gpu::WorkSize{ work_group_size, buffer_sums.number() }, buffer_sums);
+
+                buffers_scanned_sums.emplace_back();
+                auto& buffer_scanned_sums = buffers_scanned_sums.back();
+                buffer_scanned_sums.resizeN(buffer_size);
+            } while (buffer_size > work_group_size);
+
+            gpu::gpu_mem_32i max_value;
+            max_value.resizeN(1);
+            gpu::gpu_mem_32i max_idx;
+            max_idx.resizeN(1);
+
+            int max_sum;
+            int result;
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                scan.exec(
+                    gpu::WorkSize{ work_group_size, buffer_input.number() },
+                    buffer_input, buffer_output, buffers_sums[0],
+                    ocl::LocalMem{ sizeof(int) * work_group_size });
+                if (buffer_input.number() > work_group_size) {
+                    for (int i = 0; i < buffers_sums.size() - 1; ++i) {
+                        scan.exec(
+                            gpu::WorkSize{ work_group_size, buffers_sums[i].number() },
+                            buffers_sums[i], buffers_scanned_sums[i], buffers_sums[i + 1],
+                            ocl::LocalMem{ sizeof(int) * work_group_size });
+                    }
+                    scan.exec(
+                        gpu::WorkSize{ work_group_size, buffers_sums[buffers_sums.size() - 1].number() },
+                        buffers_sums[buffers_sums.size() - 1],
+                        buffers_scanned_sums[buffers_sums.size() - 1],
+                        gpu::gpu_mem_32i{},
+                        ocl::LocalMem{ sizeof(int) * work_group_size });
+                    for (int i = buffers_sums.size() - 1; i > 0; --i) {
+                        add.exec(
+                            gpu::WorkSize{ work_group_size, buffers_scanned_sums[i - 1].number() },
+                            buffers_scanned_sums[i - 1],
+                            buffers_scanned_sums[i]);
+                    }
+                    add.exec(
+                        gpu::WorkSize{ work_group_size, buffer_output.number() },
+                        buffer_output, buffers_scanned_sums[0]);
+                }
+
+                max_sum = 0;
+                result = 0;
+
+                
+                int initial_max_value = 0;
+                max_value.writeN(&initial_max_value, 1);
+
+                get_max_value.exec(
+                    gpu::WorkSize{ work_group_size, buffer_output.number() },
+                    buffer_output, max_value,
+                    ocl::LocalMem{ sizeof(int) * work_group_size });
+
+                max_value.readN(&max_sum, 1);
+
+                if (max_sum > 0) {
+                    int initial_idx_value = round_up_work_size(n) + 1;
+                    max_idx.writeN(&initial_idx_value, 1);
+
+                    get_idx_of_max.exec(
+                        gpu::WorkSize{ work_group_size, buffer_output.number() },
+                        buffer_output, max_value, max_idx);
+
+                    max_idx.readN(&result, 1);
+                }
+
+                EXPECT_THE_SAME(reference_max_sum, max_sum, "GPU result diverges");
+                EXPECT_THE_SAME(reference_result, result, "GPU result diverges");
+                t.nextLap();
+            }
+
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        } catch (const std::runtime_error& e) {
+            std::cerr << e.what() << std::endl;
+            return EXIT_FAILURE;
         }
     }
 }
