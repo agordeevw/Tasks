9851eea09227a53ce4c911ab240538107870b855
agordeevw <agordeevw@gmail.com>
2018-10-06 22:21:55 +0700

    Reduced M and K to 4096 (M = K = 8192 results are worse)

--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -19,8 +19,8 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1 << 13;
-    unsigned int K = 1 << 13;
+    unsigned int M = 1 << 12;
+    unsigned int K = 1 << 12;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
