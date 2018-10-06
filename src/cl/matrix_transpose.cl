13e251fc9aac1f30ff28fb55f8634fd9dbdd7030
agordeevw <agordeevw@gmail.com>
2018-10-06 20:47:14 +0700

    Implemented matrix transpose

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,60 @@
-__kernel void matrix_transpose(...)
+__kernel void matrix_transpose(
+  __global const float* as,
+  __global float* as_t,
+  const unsigned int rows,
+  const unsigned int cols)
 {
-    // TODO
+  const unsigned int 
+    l_i = get_local_id(0),
+    l_j = get_local_id(1),
+    g_i = get_global_id(0),
+    g_j = get_global_id(1);
+
+#ifdef TRIVIAL_SOLUTION
+  as_t[g_j + rows * g_i] = as[g_i + cols * g_j];
+#else
+  __local float l_as[TILE_SIZE * TILE_SIZE];
+
+  // assuming WARP_SIZE=32 and CACHE_LINE_SIZE=128
+  // in one warp:
+  // g_j is constant, while g_i varies in range
+  // [g_i * TILE_SIZE / TILE_SIZE, g_i * TILE_SIZE / TILE_SIZE + TILE_SIZE)
+  // l_j is constant, while l_i varies in range [0, 32)
+
+  // coalesced memory access:
+  // g_i + cols * g_j requests 128 sequential bytes
+  l_as[l_i + TILE_SIZE * l_j] = as[g_i + cols * g_j];
+  
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  // trivial approach from lecture leads to bank conflicts
+  // each thread from one warp writes to one column of the small matrix
+  // to avoid that, each warp should access not a row of the small matrix,
+  //  but a diagonal
+  // which diagonal is chosen is defined by l_j
+  // using this approach accesses to memory banks can be more uniformly distributed
+  if ((l_j > 0 && l_j < TILE_SIZE / 2) 
+      || (l_j == TILE_SIZE / 2 && l_i < TILE_SIZE / 2))
+  {
+    const unsigned int i = l_i;
+    const unsigned int j = (l_i + l_j) % TILE_SIZE;
+    float tmp = l_as[i + TILE_SIZE * j];
+    l_as[i + TILE_SIZE * j] = l_as[j + TILE_SIZE * i];
+    l_as[j + TILE_SIZE * i] = tmp;
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  const unsigned int group_id_i = g_i / TILE_SIZE;
+  const unsigned int group_id_j = g_j / TILE_SIZE;
+
+  // coalesced memory access:
+  // to achieve coalescence a block of output matrix must be chosen so that
+  // small matrix can be flushed to the block directly, row by row
+  // as established earlier, l_j is constant through the warp,
+  // while l_i changes in range [0, 32), which leads to coalesced memory access
+  const unsigned int out_g_i = group_id_j * TILE_SIZE + l_i;
+  const unsigned int out_g_j = group_id_i * TILE_SIZE + l_j;
+  as_t[out_g_i + rows * out_g_j] = l_as[l_i + TILE_SIZE * l_j];
+#endif
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -10,7 +10,6 @@
 #include <iostream>
 #include <stdexcept>
 
-
 int main(int argc, char **argv)
 {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
@@ -20,8 +19,8 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int M = 1 << 13;
+    unsigned int K = 1 << 13;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
@@ -32,44 +31,84 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
 
     as_gpu.writeN(as.data(), M*K);
 
-    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
-    matrix_transpose_kernel.compile();
+    constexpr auto TILE_SIZE = 32u;
+    std::string defines;
+    defines += " -DTILE_SIZE=";
+    defines += std::to_string(TILE_SIZE);
 
-    {
+#ifdef NDEBUG
+    const bool show_compilation_log = false;
+#else
+    const bool show_compilation_log = true;
+#endif
+
+    try {
+      std::string trivial_defines = defines;
+      trivial_defines += " -DTRIVIAL_SOLUTION";
+      ocl::Kernel matrix_transpose_kernel_trivial(matrix_transpose, matrix_transpose_length,
+        "matrix_transpose", trivial_defines);
+      matrix_transpose_kernel_trivial.compile(show_compilation_log);
+
+      ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length,
+        "matrix_transpose", defines);
+      matrix_transpose_kernel.compile(show_compilation_log);
+
+      auto run_kernel = [TILE_SIZE, M, K, benchmarkingIters, &as_gpu, &as_t_gpu, &as_t](
+        ocl::Kernel& kernel, const std::string& name) 
+      {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+          const auto rows_count = M;
+          const auto cols_count = K;
 
-            t.nextLap();
+          kernel.exec(
+            gpu::WorkSize{ TILE_SIZE, TILE_SIZE, cols_count, rows_count },
+            as_gpu, as_t_gpu, rows_count, cols_count);
+
+          t.nextLap();
         }
+        std::cout << name << std::endl;
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
-    }
+        std::cout << "GPU: " << M * K / 1000.0 / 1000.0 / t.lapAvg() << " millions/s" << std::endl;
+      
+        as_t_gpu.readN(as_t.data(), M * K);
+      };
 
-    as_t_gpu.readN(as_t.data(), M*K);
-
-    // Проверяем корректность результатов
-    for (int j = 0; j < M; ++j) {
-        for (int i = 0; i < K; ++i) {
+      auto check_correctness = [M, K, &as, &as_t]() -> bool 
+      {
+        for (unsigned j = 0; j < M; ++j) {
+          for (unsigned i = 0; i < K; ++i) {
             float a = as[j * K + i];
             float b = as_t[i * M + j];
-            if (a != b) {
-                std::cerr << "Not the same!" << std::endl;
-                return 1;
-            }
+            if (a != b)
+              return false;
+          }
         }
+        return true;
+      };
+
+      run_kernel(matrix_transpose_kernel_trivial, "Trivial solution");
+      if (!check_correctness()) {
+        std::cerr << "Not the same!" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+      run_kernel(matrix_transpose_kernel, "Local memory solution");
+      if (!check_correctness()) {
+        std::cerr << "Not the same!" << std::endl;
+        return EXIT_FAILURE;
+      }
+
+    } catch (const std::runtime_error& e) {
+      std::cerr << e.what() << std::endl;
+      return EXIT_FAILURE;
     }
-    */
 
     return 0;
 }
