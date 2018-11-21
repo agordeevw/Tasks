e2ac61eb66820a9230e73789e54d19eafa9401c4
Alexander Gordeev <agordeevw@gmail.com>
2018-11-21 10:58:05 +0700

    Implemented radix sort

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,160 @@
-__kernel void radix(__global unsigned int* as)
+// http://www.heterogeneouscompute.org/wordpress/wp-content/uploads/2011/06/RadixSort.pdf
+
+#define BITS    4
+#define BITS2   16
+#define WG_SIZE 128
+
+__kernel void count(
+    __global const uint * input,
+    const uint input_size,
+    __global uint * global_counts,
+    __global uint * global_offsets,
+    __local uint * local_counts,
+    const uint low_bit
+)
 {
-    // TODO
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+
+    if (lid < BITS2) {
+        local_counts[lid] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (gid < input_size) {
+        const uint value = input[gid];
+        uint bucket = (value >> low_bit) & ((1 << BITS) - 1);
+        atomic_inc(&local_counts[bucket]);
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (lid < BITS2) {
+        global_counts[BITS2 * get_group_id(0) + lid] = local_counts[lid];
+        if (get_group_id(0) == get_num_groups(0) - 1) {
+            global_offsets[lid] = local_counts[lid];
+        }
+    }
+}
+
+__kernel void scan(
+    __global const uint * block_offsets,
+    __global uint * global_offsets,
+    const uint block_count
+)
+{
+    __global const uint * last_block_offsets =
+        block_offsets + BITS2 * (block_count - 1);
+
+    uint sum = 0;
+    for (uint i = 0; i < BITS2; ++i) {
+        uint x = global_offsets[i] + last_block_offsets[i];
+        global_offsets[i] = sum;
+        sum += x;
+    }
+}
+
+__kernel void scatter(
+    __global const uint * input,
+    const uint input_size,
+    const uint low_bit,
+    __global const uint * counts,
+    __global const uint * global_offsets,
+    __global uint * output
+)
+{
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+
+    uint value;
+    uint bucket;
+    __local uint local_input[WG_SIZE];
+    if (gid < input_size) {
+        value = input[gid];
+        bucket = (value >> low_bit) & ((1 << BITS) - 1);
+        local_input[lid] = bucket;
+    }
+
+    __local uint local_counts[1 << BITS];
+    if (lid < BITS2) {
+        local_counts[lid] = counts[get_group_id(0) * BITS2 + lid];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (gid >= input_size)
+        return;
+
+    uint offset = global_offsets[bucket] + local_counts[bucket];
+
+    uint local_offset = 0;
+    for (uint i = 0; i < lid; i++) {
+        if (local_input[i] == bucket)
+            local_offset++;
+    }
+
+    output[offset + local_offset] = value;
+}
+
+__kernel void exclusive_scan_pre(
+    __global const uint16 * in,
+    __global uint16 * out,
+    __global uint16 * sums,
+    __local uint16 * shared
+)
+{
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+    const uint wg = get_local_size(0);
+
+    int offset = 1;
+    shared[2 * lid] = in[2 * gid];
+    shared[2 * lid + 1] = in[2 * gid + 1];
+
+    for (int d = wg; d > 0; d /= 2)
+    {
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if (lid < d)
+        {
+            int ai = offset * (2 * lid + 1) - 1;
+            int bi = offset * (2 * lid + 2) - 1;
+            shared[bi] += shared[ai];
+        }
+        offset *= 2;
+    }
+
+    if (lid == 0) {
+        if (sums)
+            sums[get_group_id(0)] = shared[2 * wg - 1];
+        shared[2 * wg - 1] = 0;
+    }
+
+    for (int d = 1; d < 2 * wg; d *= 2)
+    {
+        offset /= 2;
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if (lid < d)
+        {
+            int ai = offset * (2 * lid + 1) - 1;
+            int bi = offset * (2 * lid + 2) - 1;
+            uint16 t = shared[ai];
+            shared[ai] = shared[bi];
+            shared[bi] += t;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    out[2 * gid] = shared[2 * lid];
+    out[2 * gid + 1] = shared[2 * lid + 1];
+}
+
+__kernel void exclusive_scan_post(
+    __global const uint16 * sums,
+    __global uint16 * scanned
+)
+{
+    const uint gid = get_global_id(0);
+
+    uint16 value = sums[get_group_id(0)];
+    scanned[2 * gid] += value;
+    scanned[2 * gid + 1] += value;
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -23,6 +23,13 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+unsigned int roundup(const unsigned int value, const unsigned int n) {
+    return (value + n - 1) / n * n;
+}
+
+unsigned int wg_count(const unsigned int gw_size, const unsigned int wg_size) {
+    return roundup(gw_size, wg_size) / wg_size;
+}
 
 int main(int argc, char **argv)
 {
@@ -52,36 +59,150 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
+    constexpr unsigned int RADIX_BITS = 4;
+    constexpr unsigned int RADIX_BITS2 = 16;
+
+    const unsigned int workGroupSize = 128;
+    const unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
 
+    gpu::gpu_mem_32u output;
+    output.resizeN(n);
+
+    gpu::gpu_mem_32u counts;
+    counts.resizeN(global_work_size / workGroupSize * RADIX_BITS2);
+
+    gpu::gpu_mem_32u offsets;
+    offsets.resizeN(RADIX_BITS2);
+
+    std::vector<gpu::gpu_mem_32u> exclusive_scan_buffers;
+    unsigned int scan_work_size = counts.number();
+    while (scan_work_size > 2 * workGroupSize) {
+        scan_work_size = 16 * wg_count(scan_work_size / 2, workGroupSize);
+        gpu::gpu_mem_32u buffer;
+        buffer.resizeN(scan_work_size);
+        exclusive_scan_buffers.push_back(std::move(buffer));
+    }
+
+    gpu::gpu_mem_32u * input_buffer = &as_gpu;
+    gpu::gpu_mem_32u * output_buffer = &output;
+
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        ocl::Kernel count_kernel(radix_kernel, radix_kernel_length, "count");
+        count_kernel.compile();
+
+        ocl::Kernel scan_kernel(radix_kernel, radix_kernel_length, "scan");
+        scan_kernel.compile();
+
+        ocl::Kernel scatter_kernel(radix_kernel, radix_kernel_length, "scatter");
+        scatter_kernel.compile();
+
+        ocl::Kernel exclusive_scan_pre_kernel(radix_kernel, radix_kernel_length, "exclusive_scan_pre");
+        exclusive_scan_pre_kernel.compile();
+
+        ocl::Kernel exclusive_scan_post_kernel(radix_kernel, radix_kernel_length, "exclusive_scan_post");
+        exclusive_scan_post_kernel.compile();
 
         timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (int iter = 0; iter < 1; ++iter) {
             as_gpu.writeN(as.data(), n);
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            for (int b = 0; b < sizeof(unsigned int) * 8 / RADIX_BITS; b++) {
+                // local histograms
+                count_kernel.exec(gpu::WorkSize{ workGroupSize, global_work_size },
+                    *input_buffer,
+                    n,
+                    counts,
+                    offsets,
+                    ocl::LocalMem{ sizeof(unsigned int) * RADIX_BITS2 },
+                    b * RADIX_BITS);
+
+                // scan histograms
+                exclusive_scan_pre_kernel.exec(
+                    gpu::WorkSize{
+                        workGroupSize / 16,
+                        (uint)counts.number() / 2 / 16
+                    },
+                    counts,
+                    counts,
+                    exclusive_scan_buffers[0],
+                    ocl::LocalMem{ 2 * sizeof(unsigned int) * workGroupSize });
+
+                for (uint i = 0; i < exclusive_scan_buffers.size() - 1; ++i) {
+                    exclusive_scan_pre_kernel.exec(
+                        gpu::WorkSize{
+                            workGroupSize / 16,
+                            (uint)exclusive_scan_buffers[i].number() / 2 / 16
+                        },
+                        exclusive_scan_buffers[i],
+                        exclusive_scan_buffers[i],
+                        exclusive_scan_buffers[i+1],
+                        ocl::LocalMem{ 2 * sizeof(unsigned int) * workGroupSize });
+                }
+
+                const uint last_buffer = exclusive_scan_buffers.size() - 1;
+                exclusive_scan_pre_kernel.exec(
+                    gpu::WorkSize{
+                        workGroupSize / 16,
+                        (uint)exclusive_scan_buffers[last_buffer].number() / 2 / 16
+                    },
+                    exclusive_scan_buffers[last_buffer],
+                    exclusive_scan_buffers[last_buffer],
+                    gpu::gpu_mem_32u{},
+                    ocl::LocalMem{ 2 * sizeof(unsigned int) * workGroupSize });
+
+                for (uint i = last_buffer; i > 0; i--) {
+                    exclusive_scan_post_kernel.exec(
+                        gpu::WorkSize{
+                            workGroupSize / 16,
+                            (uint)exclusive_scan_buffers[i-1].number() / 2 / 16
+                        },
+                        exclusive_scan_buffers[i],
+                        exclusive_scan_buffers[i-1]);
+                }
+                exclusive_scan_post_kernel.exec(
+                    gpu::WorkSize{
+                        workGroupSize / 16,
+                        (uint)counts.number() / 2 / 16
+                    },
+                    exclusive_scan_buffers[0],
+                    counts);
+
+                // calc global offsets
+                scan_kernel.exec(gpu::WorkSize{ 1, 1 },
+                    counts,
+                    offsets,
+                    global_work_size / workGroupSize);
+
+                // scatter according to global offsets
+                scatter_kernel.exec(gpu::WorkSize{ workGroupSize, global_work_size },
+                    *input_buffer,
+                    n,
+                    b * RADIX_BITS,
+                    counts,
+                    offsets,
+                    *output_buffer);
+
+                std::swap(input_buffer, output_buffer);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
 
-        as_gpu.readN(as.data(), n);
+        input_buffer->readN(as.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
